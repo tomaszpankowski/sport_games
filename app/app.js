29dcf1d35f85ee2364c2766b3b223a89bc1ed2d2
tomaszpankowski <--get-all>
2022-02-04 15:01:07 +0100

    routing modified

--- app/app.js
+++ app/app.js
@@ -1,6 +1,11 @@
 var appCfg ={
     navRoutes:[        
-        {navItem:true, path:"/",name:"Home",view:"app/views/comingsoon.html"},
+        {navItem:true, path:"/",name:"Home",view:"app/views/index.html"},
+        {navItem:true, path:"/games",name:"Games",view:"app/views/games.html"},
+        {navItem:true, path:"/calendar",name:"Calendar",view:"app/views/calendar.html"},
+        {navItem:true, path:"/medals",name:"Medals",view:"app/views/medals.html"},
+        {navItem:true, path:"/news",name:"News",view:"app/views/news.html"},
+        {navItem:false, path:"/comingsoon",name:"Comingsoon",view:"app/views/comingsoon.html"},
         {navItem:true, path:"/contact",name:"Contact",view:"app/views/contact.html"},
         {navItem:true, path:"/login",name:"<span class='fa fa-user'></span>",view:"app/views/login.html",backLink:"/register"},
         {navItem:false, path:"/register",name:"Register",view:"app/views/register.html",backLink:"/login"},
